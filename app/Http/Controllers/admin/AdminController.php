f39b8677c0f197b9683a9a37fe39dadc470a7c4e
Benard Chumba <chumba101benard@gmail.com>
2025-03-12 23:14:55 +0300

    feat: update admin dashboard to display total counts for blogs and events

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers\admin;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use App\Models\Destination;
+use App\Models\Event;
 
 class AdminController extends Controller
 {
     public function dashboard(): View
     {
         $destination_count = Destination::query()->count();
+        $blog_count = Blog::query()->count();
+        $events_count = Event::query()->count();
 
-        return view("admin.dashboard.index", compact("destination_count"));
+        return view("admin.dashboard.index", compact("destination_count", "blog_count", "events_count"));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,12 +3,16 @@
         <div class="container mx-auto sm:px-6 lg:px-8 bg-white rounded">
             <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
                 <div class="relative p-5 border rounded-lg   z-0 text-primary">
-                    <p class="text-2xl font-extrabold">Total Destinations</p>
+                    <p class="text-xl font-extrabold">Destinations</p>
                     <p class="py-5 text-3xl font-bold">{{$destination_count}}</p>
                 </div>
-                <div class="p-5 border rounded-lg">
+                <div class="relative p-5 border rounded-lg   z-0 text-primary">
+                    <p class="text-2xl font-extrabold">Blogs</p>
+                    <p class="py-5 text-3xl font-bold">{{$blog_count}}</p>
                 </div>
-                <div class="p-5 border rounded-lg">
+                <div class="relative p-5 border rounded-lg   z-0 text-primary">
+                    <p class="text-2xl font-extrabold">Events</p>
+                    <p class="py-5 text-3xl font-bold">{{$events_count}}</p>
                 </div>
 
             </div>
