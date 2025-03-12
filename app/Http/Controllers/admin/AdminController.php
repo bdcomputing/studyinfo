09b982f69794812451f2b15877ef58e40450ce09
Benard Chumba <chumba101benard@gmail.com>
2025-03-12 23:05:26 +0300

    feat: display total destinations count on admin dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\admin;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
+use App\Models\Destination;
 
 class AdminController extends Controller
 {
     public function dashboard(): View
     {
-        return view("admin.dashboard.index");
+        $destination_count = Destination::query()->count();
+
+        return view("admin.dashboard.index", compact("destination_count"));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,10 +1,16 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="container mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-primary ">
-                    {{ __("You're logged in!") }}
+        <div class="container mx-auto sm:px-6 lg:px-8 bg-white rounded">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
+                <div class="relative p-5 border rounded-lg   z-0 text-primary">
+                    <p class="text-2xl font-extrabold">Total Destinations</p>
+                    <p class="py-5 text-3xl font-bold">{{$destination_count}}</p>
                 </div>
+                <div class="p-5 border rounded-lg">
+                </div>
+                <div class="p-5 border rounded-lg">
+                </div>
+
             </div>
         </div>
     </div>
