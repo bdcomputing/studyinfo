f0c372d4ebf03abda253f344c3803842a0bdb512
Benard Chumba <chumba101benard@gmail.com>
2025-03-13 16:22:59 +0300

    refactor: update contact page layout with improved styling and icons

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -88,53 +88,27 @@ Contact
                 <div class="rounded-lg pb-5">
                     <img class="object-fit w-full h-96 rounded-3xl" src="/assets/images/contact.png" alt="">
                 </div>
-                <div class="bg-white shadow-lg rounded-3xl p-8">
-                    <div class="space-y-4">
-                        <div class="flex items-center bg-white shadow-lg rounded-lg p-5">
-                            <div class="h-12 w-12 bg-primary-100 rounded-full flex items-center justify-center mr-4">
-                                <svg class="w-6 h-6 text-primary-900" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
-                                    </path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                </svg>
+                <div class="bg-white rounded-3xl p-8">
+                    <div class="">
+                        <div class="space-y-3 rounded-lg p-5">
+                            <div class="flex items-center gap-3">
+                                <i class="bi bi-geo-alt text-2xl"></i> Address
                             </div>
-                            <div class="grid grid-cols-1 gap-1">
-                                <span class="text-secondary-950">Address</span>
-                                <span class="text-secondary-400">64 PLAZA, Eldoret-Kisumu Rd, ELDORET</span>
-                            </div>
+                            <p class="text-gray-600 text-sm">Daima Towers, Uganda Rd, ELDORET</p>
                         </div>
-                        <div class="flex items-center bg-white shadow-lg rounded-lg p-5">
-                            <div class="h-12 w-12 bg-primary-100 rounded-full flex items-center justify-center mr-4">
-                                <svg class="w-6 h-6 text-primary-500" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
-                                    </path>
-                                </svg>
+                        <div class="space-y-3 rounded-lg p-5">
+                            <div class="flex items-center gap-3">
+                                <i class="bi bi-telephone text-2xl"></i> Phone
                             </div>
-                            <div class="grid grid-cols-1 gap-1">
-                                <span class="text-secondary-950">Phone</span>
-                                <span class="text-secondary-400">+254 799 965 066</span>
-                            </div>
+                            <p class="text-gray-600 text-sm">+254 799 965 066</p>
                         </div>
-                        <div class="flex items-center bg-white shadow-lg rounded-lg p-5">
-                            <div class="h-12 w-12 bg-primary-100 rounded-full flex items-center justify-center mr-4">
-                                <svg class="w-6 h-6 text-primary-500" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-                                    </path>
-                                </svg>
+                        <div class="space-y-3 rounded-lg p-5">
+                            <div class="flex items-center gap-3">
+                                <i class="bi bi-envelope-at text-2xl"></i> Email
                             </div>
+                            <p class="text-gray-600 text-sm">info@haitrac.com</p>
+                        </div>
 
-                            <div class="grid grid-cols-1 gap-1">
-                                <span class="text-secondary-950">Email</span>
-                                <span class="text-secondary-400">info@haitrac.com</span>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
@@ -163,7 +137,7 @@ Contact
             <div class="container mx-auto py-10">
                 <div class="pb-5 px-4">
                     <p class="text-2xl font-semibold">How to Find Us</p>
-                    <p class="text-sm text-secondary-300">
+                    <p class="text-sm">
                         Use the map below to locate our offices
                     </p>
                 </div>
