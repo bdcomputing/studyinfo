4a09c17a0a2c0d6104585e175fa756d93aaae470
Benard Chumba <chumba101benard@gmail.com>
2025-03-18 14:41:43 +0300

    feat: add City model and migration for cities table with relationships and attributes

---
+++ app/Models/City.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+
+    use HasFactory;
+    //
+    protected $fillable = [
+        "name",
+        "destination_id",
+        "state",
+        "timezone",
+        "population",
+    ];
+
+    protected $casts = [
+        "destination_id" => "numeric",
+    ];
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class);
+    }
+}
